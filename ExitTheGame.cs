1b06a972367d05a0ac017f8ab560fbf98fd1c1e8
DevDuck33 <game.dev@op.pl>
2019-01-01 22:39:54 +0100

    0.0.2
    -Exit The Game
    - Simple Menu Scene

---
+++ ExitTheGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitTheGame : MonoBehaviour
+{
+    public GameObject ExitPanel;
+    public void OpenExit()
+    {
+        ExitPanel.SetActive(true);
+    }
+
+    public void CloseExit()
+    {
+        ExitPanel.SetActive(false);
+    }
+
+    public void LeaveTheGame()
+    {
+        Application.Quit();
+    }
+}
